f9790334c09a8fa3dbb225f6b7268a071f9e0eb2
williamsadu-gyamfi <adugyamfiwilliams23@gmail.com>
2024-07-15 21:57:32 -0700

    initial commit

---
+++ starter/script.js
@@ -0,0 +1,9 @@
+"use strict";
+const showModalbtn = document.querySelectorAll(".show-modal");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const closebtn = document.querySelector(".close-modal");
+console.log(showModalbtn);
+for (let i = 0; i < showModalbtn; i++) {
+  console.log(showModalbtn[i].textContent);
+}
